0baabc4a3627dbe131e82542aa3050651c8a5e26
Verbatiwm <cervoneaca@gmail.com>
2026-04-01 12:07:35 +0300

    First Comm

---
+++ app/src/main/java/com/example/playlistmaker/MediaActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication1
+
+import android.os.Bundle
+import android.widget.ImageView
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.playlistmaker.R
+
+
+class MediaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_media)
+        val back = findViewById<ImageView>(R.id.back_button)
+
+        back.setOnClickListener {
+            finish()
+        }
+    }
+}
