e6fb39e14dc8d6d46106736d95a7289b8aa181fc
Verbatiwm <cervoneaca@gmail.com>
2026-04-03 17:25:21 +0300

    Sprint 8 review fixed

--- app/src/main/java/com/example/playlistmaker/MediaActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MediaActivity.kt
@@ -2,19 +2,21 @@ package com.example.myapplication1
 
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
 
 import androidx.appcompat.app.AppCompatActivity
 import com.example.playlistmaker.R
+import com.google.android.material.appbar.MaterialToolbar
 
 
 class MediaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContentView(R.layout.activity_media)
-        val back = findViewById<ImageView>(R.id.back_button)
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
 
-        back.setOnClickListener {
+        toolbar.setNavigationOnClickListener {
             finish()
         }
     }
